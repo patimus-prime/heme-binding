aff50048bc37aceced0e546b2a9235402093f116
patimus-prime <patrick.finnerty42@gmail.com>
2021-08-10 03:27:37 +0200

    blah

--- scripts/py/main.py
+++ scripts/py/main.py
@@ -1,5 +1,22 @@
 # main.py
+import os
+import os.path
 
+# import FILENAME, this requires referncing fn's as FILENAME.fn()
+# from FILENAME import *, this refernces fn's as fn()
+# therefore we require to alter the scripts to be functions. let's get current functionality first.
+
+# ligandList = "HEM,HEC,SRM,VER,VEA:"
+# angstromDistance =
+# sourcePath =
+
+import convert_multi_to_mono
+import calc_volume
+#import calc_residues...
+
+
+sourcePath = "~/heme-binding/pdb_source_data/0_raw_download"
+sourcePath = os.path.expanduser(sourcePath)
 
 
 
